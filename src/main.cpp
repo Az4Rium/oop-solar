7b43e00f7645fdedfbe12fd128da1c6c5c38fd8d
Alexander <zsashok.ok@gmail.com>
2026-02-03 21:39:15 +0300

    Floating Non resiable window

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/WindowEnums.hpp>
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+  sf::RenderWindow window(sf::VideoMode({640, 480}), "SFML works!",
+                          sf::Style::Close);
+  window.setPosition({0, 0});
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
 
+  shape.setPosition({320 - 100.0f, 240 - 100.0f});
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>())
